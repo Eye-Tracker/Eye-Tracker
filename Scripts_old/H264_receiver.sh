7f586c51da98b93c112f2c120f73a5f76bc02c51
CORaisch <claudio.raisch@hotmail.de>
2017-08-02 13:56:43 +0200

    Deleted old Files

--- Scripts_old/H264_receiver.sh
+++
@@ -1,1 +0,0 @@
-gst-launch-1.0 udpsrc port=5200 ! application/x-rtp, encoding-name=H264, payload=96 ! rtph264depay ! h264parse ! avdec_h264 ! autovideosink -v
--- Scripts_old/motionjpeg_test_receiver.sh
+++
@@ -1,1 +0,0 @@
-gst-launch-1.0 udpsrc port=5200 ! rtpjpegdepay ! jpegdec ! videoconvert ! ximagesink sync=false -v
